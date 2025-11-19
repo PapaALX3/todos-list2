79d20aaf0c8e71f7af2fdb836e33006cfa2c389b
Alex C <193105134+PapaALX3@users.noreply.github.com>
2025-11-18 19:48:02 -0800

    components week3

---
+++ src/TodoForm.jsx
@@ -0,0 +1,11 @@
+function TodoForm() {
+    return (
+        <form>
+            <label htmlFor="todoTitle">Todo</label>
+            <input type="text" />
+            <button>Add Todo</button>
+        </form>
+    ) 
+}
+  
+export default TodoForm
